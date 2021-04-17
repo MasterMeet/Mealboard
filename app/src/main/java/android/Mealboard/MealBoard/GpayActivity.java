3fc246fc7630525ea2e3833f48069ed77aecffc9
MasterMeet <marutimeet@gmail.com>
2021-04-17 17:55:14 +0530

    'BugFix'

---
+++ app/src/main/java/android/Mealboard/MealBoard/GpayActivity.java
@@ -0,0 +1,14 @@
+package android.Mealboard.MealBoard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GpayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpay);
+    }
+}
